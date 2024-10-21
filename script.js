99a791cdf7f0bc00996a46bcc541325b8d864db8
jumin20099 <jumin20099@gmail.com>
2024-10-22 02:42:32 +0900

    헤더 캐러셀 수정

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CAROUSEL_LENGTH = Math.ceil(document.querySelectorAll(".product-img").length / 3) - 1; // 전체 페이지 수
+const CAROUSEL_LENGTH = 6;
 let current = 0;
 const $carousel = document.querySelector("#modal-product");
 const $prevButton = document.querySelector(".prev-button");
@@ -8,14 +8,14 @@ const nextEvent = () => {
     if (current < CAROUSEL_LENGTH) {
         current++;
     }
-    $carousel.style.transform = `translateX(${-current * 250}px)`; // 450px 이동 (상품 하나의 넓이)
+    $carousel.style.transform = `translateX(${-current * 300}px)`; // 450px 이동 (상품 하나의 넓이)
 };
 
 const prevEvent = () => {
     if (current > 0) {
         current--;
     }
-    $carousel.style.transform = `translateX(${-current * 250}px)`; // 450px 이동 (상품 하나의 넓이)
+    $carousel.style.transform = `translateX(${-current * 300}px)`; // 450px 이동 (상품 하나의 넓이)
 };
 
 $nextButton.addEventListener("click", nextEvent);
